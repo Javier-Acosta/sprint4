55ead3d94efa3e5d142f9819e439bf647cb21716
EsteNoEsMonti <estenoesmonti@gmail.com>
2025-03-28 13:12:51 -0300

    LKS correcciones

--- src/components/PersonajeCard.jsx
+++ src/components/PersonajeCard.jsx
@@ -1,18 +1,34 @@
 import React from 'react'
-import { usePersonajes } from '../context/personajesContext'
+// ⚠️⚠️⚠️ ----------------------------------------------------------------------------------------------------------------------------------- ⚠️⚠️⚠️
+// import { usePersonajes } from '../context/personajesContext' // LSK - mismo error que en searchForm, lo arreglé aquí también
+import { usePersonajes } from '../context/PersonajesContext'
+// ⚠️⚠️⚠️ ----------------------------------------------------------------------------------------------------------------------------------- ⚠️⚠️⚠️
 
 const PersonajeCard = () => {
-  const {personajeData, loading,error}=usePersonajes
-  if (loading)  return <p className='text-gray-700'>Loading...  </p>
-  if(error) return <p className='text-red-500'>{error}  </p>
-  if(!personajeData) return null
-  
-const {name, species, image, status}=personajeData
-console.log('Card',personajeData);
-    
+  // ⚠️⚠️⚠️ ----------------------------------------------------------------------------------------------------------------------------------- ⚠️⚠️⚠️
+  // const {personajeData, loading,error}=usePersonajes // LKS - aquí te faltaba el parentesis al final
+  const { personajeData, loading, error } = usePersonajes() // LKS - aquí lo agregué
+  // ⚠️⚠️⚠️ ----------------------------------------------------------------------------------------------------------------------------------- ⚠️⚠️⚠️
+
+  console.log('personajeData -> ', personajeData)
+  if (loading) return <p className='text-gray-700'>Loading...  </p>
+  if (error) return <p className='text-red-500'>{error}  </p>
+  if (!personajeData) return null
+
+  const { name, species, image, status } = personajeData
+  console.log('Card', personajeData); // LSK - aquí estas mostrando por consola el personajeData, desde el navegador podes ver que es lo que te trare
+  // así que aquí esta tu problema, no estas accediendo a los datos de la forma correcta, ya que el personajeData es un objeto, por lo tanto no tenes que acceder a la informacion
+  // que vos queres, que sería, el arreglo de personajes, que se encuentra dentro del objeto data, y luego el arreglo results, por lo tanto tenes que acceder a los datos de la siguiente manera:
+  // personajeData.data.results[0].name, personajeData.data.results[0].species, etc.
+  console.log('🌙 LKS -> personaje ', personajeData.data.results[0])
+  console.log('🌙 LKS -> personaje . name', personajeData.data.results[0].name)
+
+  // vale la aclaracion, que ahí solo estarias accediendo al primer personaje, si queres acceder a todos los personajes, tenes que hacer un map, y luego dentro del map, acceder a los datos de la misma manera que te lo dije antes
+
   return (
     <div >
-      <h2 className='text-2xl font-bold'>{name}</h2>
+      {/* <h2 className='text-2xl font-bold'>{name}</h2> */}
+      <h2 className='text-2xl font-bold'>{personajeData.data.results[0].name}</h2>
       <p className='text-2xl font-bold'>{species}</p>
       <p className='text-2xl font-bold'>{status}</p>
       PersonajeCard</div>
--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,38 +1,38 @@
+// LSK - este tambien lo indento un poco, cuidado con dejar mas de 2 lineas sin nada
 import React, { useState } from 'react'
-import { usePersonajes } from '../context/personajesContext'
+
+// ⚠️⚠️⚠️ ----------------------------------------------------------------------------------------------------------------------------------- ⚠️⚠️⚠️
+// import { usePersonajes } from '../context/personajesContext' // LKS - te marca error pq pusiste la "p" en minuscula, ya te lo puse bien en la linea de arriba 
+import { usePersonajes } from '../context/PersonajesContext'
+// ⚠️⚠️⚠️ ----------------------------------------------------------------------------------------------------------------------------------- ⚠️⚠️⚠️
 
 const SearchForm = () => {
   const [personaje, setPersonaje] = useState('')
-  const{getPersonajes}= usePersonajes()
+  const { getPersonajes } = usePersonajes()
 
-const handleSubmit=(e)=>{
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (personaje.trim() === '') return
+    console.log('boton', personaje);
 
-  e.preventDefault()
-  if (personaje.trim()==='')return
-  console.log('boton',personaje);
-  
-  getPersonajes(personaje)
-  
+    getPersonajes(personaje)
   }
 
-
   return (
-  <form onSubmit={handleSubmit} className='flex gap-2 mb-6'>
-    <input type="text"
-      value={personaje}
-      onChange={(e) => setPersonaje (e.target.value)}
-      placeholder='Entry Person'
-      className='p-2 border border-gray-300 rounded'
-    
-    />
-  <button 
-    type='submit'
-    className='bg-blue-500 text-white px-4 py-2 rounded'
-
-  >
-    Search
-  </button>
-  </form>
+    <form onSubmit={handleSubmit} className='flex gap-2 mb-6'>
+      <input type="text"
+        value={personaje}
+        onChange={(e) => setPersonaje(e.target.value)}
+        placeholder='Entry Person'
+        className='p-2 border border-gray-300 rounded'
+      />
+      <button
+        type='submit'
+        className='bg-blue-500 text-white px-4 py-2 rounded'
+      >
+        Search
+      </button>
+    </form>
   )
 }
 
