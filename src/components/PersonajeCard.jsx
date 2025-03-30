143dfaa9775d59286b0cfb25ebd8b13b595cb8aa
EsteNoEsMonti <estenoesmonti@gmail.com>
2025-03-30 19:41:32 -0300

    revisiones .map y get personajes

--- src/components/PersonajeCard.jsx
+++ src/components/PersonajeCard.jsx
@@ -17,12 +17,7 @@ const PersonajeCard = () => {
   if (!personajeData) return null
 
   const { name, species, image, status } = personajeData
-  
- 
 
- 
-
-  
   console.log('Card', personajeData); // LSK - aquí estas mostrando por consola el personajeData, desde el navegador podes ver que es lo que te trare
   // así que aquí esta tu problema, no estas accediendo a los datos de la forma correcta, ya que el personajeData es un objeto, por lo tanto no tenes que acceder a la informacion
   // que vos queres, que sería, el arreglo de personajes, que se encuentra dentro del objeto data, y luego el arreglo results, por lo tanto tenes que acceder a los datos de la siguiente manera:
@@ -32,17 +27,28 @@ const PersonajeCard = () => {
 
   // vale la aclaracion, que ahí solo estarias accediendo al primer personaje, si queres acceder a todos los personajes, tenes que hacer un map, y luego dentro del map, acceder a los datos de la misma manera que te lo dije antes
 
-
-
-  
   return (
     <div >
       <h2 className='text-2xl font-bold'>{personajeData.data.results[0].name}</h2>
-     
-       <h2 className='text-2xl font-bold'></h2>
+
+      <h2 className='text-2xl font-bold'></h2>
       {/* <h2 className='text-2xl font-b1
       ld'>{person}</h2> */}
-      <img src={personajeData.data.results[0].image} alt=""  />
+      { 
+        personajeData.data.results.length > 0 && 
+        <>
+          {personajeData.data.results.map((personaje) => (
+            <div key={personaje.id} className='mb-4'>
+              <h2 className='text-2xl font-bold'>{personaje.name}</h2>
+              <p className='text-gray-700'>Species: {personaje.species}</p>
+              <p className='text-gray-700'>Status: {personaje.status}</p>
+              <img src={personaje.image} alt="" width="50" />
+            </div>
+          ))}
+        </>
+
+      }
+      {/* <img src={personajeData.data.results[0].image} alt=""  /> */}
       
       <p className='text-2xl font-bold'></p>
       <p className='text-2xl font-bold'></p>
